cfa6e1e98d45d3c32b390ee7b97770bbc526a264
Matheus Rossetti <matheus__rossetti@hotmail.com>
2021-12-11 04:43:30 -0300

    testes

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,30 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DateUtilsTest {
+
+    @Test
+    public void deveRetornarTrueParaDatasFuturas(){
+        LocalDate date = LocalDate.of(2100,12,12);
+
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarFalseParaDatasPassadas(){
+        LocalDate date = LocalDate.of(1900,12,10);
+
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDatasAtual(){
+        LocalDate date = LocalDate.now();
+
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+}
